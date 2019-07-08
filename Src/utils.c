67089ce7e6735fff797426a3ee7a8ed897ddc8cf
Manuel Romei <manuel.romei@raytecvision.com>
2019-07-08 12:01:41 +0200

    Fix utils.c

--- Src/utils.c
+++ Src/utils.c
@@ -4,6 +4,7 @@
  *  Created on: Jul 7, 2019
  *      Author: kriive
  */
+#include "utils.h"
 
 // Implementation from -- https://www.arduino.cc/reference/en/language/functions/math/map/#_appendix
 long map(long x, long in_min, long in_max, long out_min, long out_max) {
